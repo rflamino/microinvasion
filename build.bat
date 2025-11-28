fcf40f3c2c7fb4656c9555bfd58b113febcd428b
Reinaldo S Flamino <reinaldosflamino@gmail.com>
2025-11-28 13:22:10 -0300

    clean before build batch

--- build.bat
+++ build.bat
@@ -9,4 +9,10 @@
 
 cls
 
+REM Clean the output directory (suppress confirmation with /q)
+if exist out\ (
+    echo Cleaning output directory...
+    del /q out\*.*
+)
+
 ..\..\tools\build\Node\node.exe ..\..\engine\script\js\build.js target=%1
