b71a42288ecbd4700f329566343defd874574e1b
Reinaldo S Flamino <reinaldosflamino@gmail.com>
2025-11-28 18:05:30 -0300

    batch file update

--- build.bat
+++ build.bat
@@ -9,10 +9,15 @@
 
 cls
 
-REM Clean the output directory (suppress confirmation with /q)
+REM Check if the output directory exists
 if exist out\ (
+    REM If it exists, clean it
     echo Cleaning output directory...
     del /q out\*.*
+) else (
+    REM If it doesn't exist, create it
+    echo Creating output directory...
+    mkdir out
 )
 
 ..\..\tools\build\Node\node.exe ..\..\engine\script\js\build.js target=%1
